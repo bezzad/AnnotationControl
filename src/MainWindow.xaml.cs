e8ec84e697662f6a30eebe740cd05153d8345425
Behzad <behzad.khosravifar@gmail.com>
2019-11-02 00:31:28 +0330

    clean extra codes

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Windows;
+﻿using System.Windows;
 
 namespace WPF.Core
 {
@@ -12,45 +10,6 @@ namespace WPF.Core
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            var url = "https://taaghche.com";
-            OpenUrl(url);
-        }
-
-        public void OpenUrl(string url)
-        {
-            url = url.Replace("&", "^&");
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}")
-                {
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                });
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                Process.Start("xdg-open", url);
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                Process.Start("open", url);
-            }
-            else
-            {
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = url,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    UseShellExecute = true,
-                    CreateNoWindow = true
-                });
-            }
         }
     }
 }
