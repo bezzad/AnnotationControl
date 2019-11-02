e55f46942ca8b82d9a62cfc823bb04645e185e47
Behzad <behzad.khosravifar@gmail.com>
2019-11-03 00:16:55 +0330

    fix peak pos in bottom of annotation

--- src/Annotation.cs
+++ src/Annotation.cs
@@ -17,10 +17,16 @@ namespace WPF.Core
         public static readonly DependencyProperty ForegroundProperty = DependencyProperty.Register(nameof(Foreground), typeof(Brush), typeof(Annotation), new PropertyMetadata(default(Brush)));
         public static readonly DependencyProperty BubblePeakWidthProperty = DependencyProperty.Register(nameof(BubblePeakWidth), typeof(double), typeof(Annotation), new PropertyMetadata(default(double)));
         public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(nameof(CornerRadius), typeof(double), typeof(Annotation), new PropertyMetadata(default(double)));
+        public static readonly DependencyProperty BubblePeakPositionProperty = DependencyProperty.Register(nameof(BubblePeakPosition), typeof(Point), typeof(Annotation), new PropertyMetadata(default(Point)));
 
+        public Point BubblePeakPosition
+        {
+            get => (Point)GetValue(BubblePeakPositionProperty);
+            set => SetValue(BubblePeakPositionProperty, value);
+        }
         public double CornerRadius
         {
-            get => (double) GetValue(CornerRadiusProperty);
+            get => (double)GetValue(CornerRadiusProperty);
             set => SetValue(CornerRadiusProperty, value);
         }
         public double BubblePeakWidth
@@ -53,7 +59,6 @@ namespace WPF.Core
             get => (double)GetValue(PaddingProperty);
             set => SetValue(PaddingProperty, value);
         }
-        public Point BubblePeakPosition { get; set; }
 
         public Annotation()
         {
@@ -67,11 +72,25 @@ namespace WPF.Core
             _pen = new Pen(BorderBrush, BorderThickness);
         }
 
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+
+            if (e.Property.Name == nameof(BubblePeakPosition))
+            {
+                if (BubblePeakPosition.X < CornerRadius + BubblePeakWidth)
+                    SetValue(BubblePeakPositionProperty, new Point(CornerRadius + BubblePeakWidth, BubblePeakPosition.Y < ActualHeight ? 0 : ActualHeight));
+
+                if (BubblePeakPosition.X > ActualWidth - CornerRadius - BubblePeakWidth)
+                    SetValue(BubblePeakPositionProperty, new Point(ActualWidth - CornerRadius - BubblePeakWidth, BubblePeakPosition.Y < ActualHeight ? 0 : ActualHeight));
+            }
+        }
+
         protected override void OnRender(DrawingContext drawingContext)
         {
             _pen.Brush = BorderBrush;
             _pen.Thickness = BorderThickness;
-
+            var dX = BubblePeakPosition.Y > 0 ? ActualWidth - BubblePeakPosition.X : BubblePeakPosition.X; // rotate around x axis 
             //                  d
             //                 / \
             //     b____2____c/3 4\e___5___f
@@ -87,9 +106,9 @@ namespace WPF.Core
             //
             var a = new Point(0, CornerRadius);
             var b = new Point(CornerRadius, 0);
-            var c = new Point(BubblePeakPosition.X - BubblePeakWidth / 2, 0);
-            var d = new Point(BubblePeakPosition.X, -CornerRadius);
-            var e = new Point(BubblePeakPosition.X + BubblePeakWidth / 2, 0);
+            var c = new Point(dX - BubblePeakWidth / 2, 0);
+            var d = new Point(dX, -CornerRadius);
+            var e = new Point(dX + BubblePeakWidth / 2, 0);
             var f = new Point(ActualWidth - CornerRadius, 0);
             var g = new Point(ActualWidth, 10);
             var h = new Point(ActualWidth, ActualHeight - CornerRadius);
@@ -113,7 +132,8 @@ namespace WPF.Core
             };
 
             var pthFigure = new PathFigure(a, pathSegments, false) { IsFilled = true };
-            var pthGeometry = new PathGeometry(new List<PathFigure> { pthFigure }, FillRule.Nonzero, null);
+            var transform = BubblePeakPosition.Y > 0 ? new RotateTransform(-180, ActualWidth / 2, ActualHeight / 2) : null;
+            var pthGeometry = new PathGeometry(new List<PathFigure> { pthFigure }, FillRule.EvenOdd, transform);
             drawingContext.DrawGeometry(Background, _pen, pthGeometry);
         }
     }
